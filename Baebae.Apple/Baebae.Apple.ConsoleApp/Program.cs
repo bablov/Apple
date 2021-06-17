e6a78b12f0d5c25ea48956629379b33207854ba6
BeibeiLi(@Home-PC) <baebaego@hotmail.com>
2021-06-17 23:12:42 +0800

    泛型

--- Baebae.Apple/Baebae.Apple.ConsoleApp/Program.cs
+++ Baebae.Apple/Baebae.Apple.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Baebaego.Practice.Genericity;
 using static System.Console;
 using static Baebae.Apple.ConsoleApp.SevenNewFeature;
 
@@ -10,9 +11,37 @@ namespace Baebae.Apple.ConsoleApp
         {
             WriteLine("Hello World!");
 
-            var result = IsFirstSummerMonday(DateTime.Now);
+            //var result = IsFirstSummerMonday(DateTime.Now);
 
-            WriteLine(result);
+            //WriteLine(result);
+
+            #region ·ºÐÍ
+            //var test = new Test<string, byte>("abc",255);
+
+            //var s1 = new Sample<string>();
+            //s1.Work("Hello");
+
+            //var s2 = new Sample<DateTime>();
+            //s2.Work(DateTime.Now);
+
+            //var s3 = new Sample<decimal>();
+            //s3.Work(0.33M);
+
+            //var s4 = new Sample<float>();
+            //s4.Work(11.954f);
+
+            //var s5 = new Sample<byte>();
+            //s5.Work(255);
+
+            //var s6 = new Sample<uint>();
+            //s6.Work(798652);
+
+            Something1 v1 = new Something1();
+            v1.Output(500, 99.88d);
+
+            Something2<char, string> v3 = new Something2<char, string>();
+            v3.Output('c', "cat");
+            #endregion
 
             ReadKey();
         }
---
+++ Baebae.Apple/Baebaego.Practice/Genericity/GenericityTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Baebaego.Practice.Genericity
+{
+    public class Test<T,F>
+    {
+        public Test(T a,F b)
+        {
+            WriteLine(a.GetType().Name);
+            WriteLine(b.GetType().Name);
+        }
+    }
+
+    public class Sample<K>
+    {
+        public void Work(K p)
+        {
+            WriteLine($"{p.GetType().FullName,-20}:{p}");
+        }
+    }
+
+    public interface ITest<P,Q>
+    {
+        void Output(P x, Q y);
+    }
+
+    public class Something1:ITest<int,double>
+    {
+        public void Output(int x,double y)
+        {
+            WriteLine($"{x.GetType()}-{x}");
+            WriteLine($"{y.GetType()}-{y}");
+        }
+    }
+    public class Something2<J,K> : ITest<J,K>
+    {
+        public void Output(J x, K y)
+        {
+            WriteLine($"{x.GetType()}-{x}");
+            WriteLine($"{y.GetType()}-{y}");
+        }
+    }
+
+}
